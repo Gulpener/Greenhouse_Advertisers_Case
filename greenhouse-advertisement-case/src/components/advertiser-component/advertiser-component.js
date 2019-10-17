0368b1c9e64c4c75a052ac2ec346b4f8287c9956
Gertjan Gielen <gjgielen@gmail.com>
2019-10-17 09:04:03 +0200

    Empty advertisers component

---
+++ greenhouse-advertisement-case/src/components/advertiser-component/advertiser-component.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class AdvertiserComponent extends React.Component {
+    render() {
+      return <h1>Overview of advertisers</h1>;
+    }
+  }
+
+  export default AdvertiserComponent;
