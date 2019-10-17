e380df3c4fba6bfab10659cbd3a4731309f0ccac
Gertjan Gielen <gjgielen@gmail.com>
2019-10-17 10:03:08 +0200

    Excercise 1 - Load and display advertisers

--- greenhouse-advertisement-case/src/components/advertiser-component/advertiser-component.js
+++ greenhouse-advertisement-case/src/components/advertiser-component/advertiser-component.js
@@ -5,21 +5,45 @@ class AdvertiserComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {      
-      items: [
-        {name: "Advertiser 1",createdAt: "2018-04-15T06:27:46.058Z", "campaignIds": [ 4,7,8 ]},
-        {name: "Advertiser 2",createdAt: "2018-03-13T21:50:10.926Z", "campaignIds": [ 1 ]},
-        {name: "Advertiser 3",createdAt: "2018-06-01T06:53:56.638Z", "campaignIds": [ 9, 5, 2, 3, 4 ]}
-      ]
+      items: []
     };
+  }
+
+  
+  componentDidMount() {
+    fetch("https://5b87a97d35589600143c1424.mockapi.io/api/v1/advertisers")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({            
+            items: result
+          });
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
   }
 
 
     render() {
       const { items } = this.state;
+      let tableItems;
+      if(items)
+      {
+        tableItems = (
+          items.map(item => (
+          <tr key={item.name}>
+            <td>{item.name}</td>
+            <td>{this.formatDate(item.createdAt)}</td>
+            <td>{item.campaignIds.length}</td>
+          </tr>
+      )));
+      }
       return (
         <div>
           <h1>Overview of advertisers</h1>
-          <table class="table">
+          <table className="table table-striped">
             <thead>
               <tr>
                 <th>Name</th>
@@ -28,13 +52,9 @@ class AdvertiserComponent extends React.Component {
               </tr>
             </thead>
             <tbody>
-              {items.map(item => (
-                <tr key={item.name}>
-                  <td>{item.name}</td>
-                  <td>{this.formatDate(item.createdAt)}</td>
-                  <td>{item.campaignIds.length}</td>
-                </tr>
-            ))}
+              {
+                tableItems
+              }
             </tbody>
           </table>
         </div>);
