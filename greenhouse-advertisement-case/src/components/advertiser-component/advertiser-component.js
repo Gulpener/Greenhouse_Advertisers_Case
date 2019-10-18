8f733d9050cc0cd8d84f35c2adafa925327bed08
Gertjan Gielen <gertjan.gielen@teamrockstars.nl>
2019-10-18 15:13:39 +0200

    feat: move table body to different component

--- greenhouse-advertisement-case/src/components/advertiser-component/advertiser-component.js
+++ greenhouse-advertisement-case/src/components/advertiser-component/advertiser-component.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import moment from 'moment';
-import QueryString from 'query-string'
+import QueryString from 'query-string';
 import './advertiser-component.css';
+import AdvertiserTableBodyComponent from '../advertiser-table-body-component/advertiser-table-body-component';
 
 class AdvertiserComponent extends React.Component {
   constructor(props) {
@@ -12,7 +12,7 @@ class AdvertiserComponent extends React.Component {
     if(this.props.location.search)
     {
       const parsedQueryString = QueryString.parse(this.props.location.search);
-      sort = {order: parsedQueryString.order, orderBy: parsedQueryString.orderBy};      
+      sort = {orderBy: parsedQueryString.orderBy, order: parsedQueryString.order};      
     }    
 
     this.state = {      
@@ -115,33 +115,7 @@ class AdvertiserComponent extends React.Component {
 
     render() {
       const { items, isAdvertisersLoading, isAdvertisersError, sort } = this.state;         
-      let tableContent;
-      if(isAdvertisersError)
-      {
-        tableContent = (<tr><td>Could not load data :(</td><td></td><td></td><td></td><td></td></tr>)
-      }
-      else if(isAdvertisersLoading)
-      {
-        tableContent = (<tr><td>Loading...</td><td></td><td></td><td></td><td></td></tr>)
-      }
-      else if(items)
-      {
-        let advertisers = items; 
-        if(sort)
-        {
-          advertisers = items.sort(this.valueComparer(sort.orderBy, sort.order))
-        }
-        tableContent = (
-          advertisers.map(advertiser => (
-          <tr key={advertiser.name}>
-            <td>{advertiser.name}</td>
-            <td>{this.formatDate(advertiser.createdAt)}</td>
-            <td>{advertiser.campaigns}</td>
-            <td>{advertiser.impressions ? advertiser.impressions : "n/a"}</td>
-            <td>{advertiser.clicks ? advertiser.clicks : "n/a"}</td>
-          </tr>
-        )));
-      }      
+ 
       return (            
         <div className="content">
           <h1>Overview of Advertisers</h1>
@@ -156,9 +130,12 @@ class AdvertiserComponent extends React.Component {
               </tr>
             </thead>
             <tbody>
-              {
-                tableContent
-              }
+              <AdvertiserTableBodyComponent 
+                data={items} 
+                isisAdvertisersLoading={isAdvertisersLoading}  
+                isAdvertisersError={isAdvertisersError}
+                sort={sort}>
+                </AdvertiserTableBodyComponent>
             </tbody>
           </table>
         </div>
@@ -216,33 +193,6 @@ class AdvertiserComponent extends React.Component {
       this.setState({
         sort: sort 
       });
-    }
-
-    formatDate(string){
-      return new moment(string).format("DD-MM-YYYY");
-    }
-    
-     valueComparer(key, order='asc') {
-      return function(a, b) {
-        if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {          
-          return 0;
-        }
-
-        const varA = (typeof a[key] === 'string') ?
-          a[key].toUpperCase() : a[key];
-        const varB = (typeof b[key] === 'string') ?
-          b[key].toUpperCase() : b[key];
-
-        let comparison = 0;
-        if (varA > varB) {
-          comparison = 1;
-        } else if (varA < varB) {
-          comparison = -1;
-        }
-        return (
-          (order === 'desc') ? (comparison * -1) : comparison
-        );
-      };
     }
   }
 
---
+++ greenhouse-advertisement-case/src/components/advertiser-table-body-component/advertiser-table-body-component.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import moment from 'moment';
+
+class AdvertiserTableBodyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+
+    }
+
+    render(){
+        const { data, isAdvertisersLoading, isAdvertisersError, sort } = this.props;         
+        console.log(this.props);
+        if(isAdvertisersError)
+        {
+          return (<tr><td>Could not load data :(</td><td></td><td></td><td></td><td></td></tr>)
+        }
+        else if(isAdvertisersLoading)
+        {
+          return (<tr><td>Loading...</td><td></td><td></td><td></td><td></td></tr>)
+        }
+        else if(data)
+        {
+          let advertisers = data; 
+          if(sort)
+          {
+            console.log(sort);
+            advertisers = data.sort(this.valueComparer(sort.orderBy, sort.order))
+          }
+          return(
+            advertisers.map(advertiser => (
+            <tr key={advertiser.name}>
+              <td>{advertiser.name}</td>
+              <td>{this.formatDate(advertiser.createdAt)}</td>
+              <td>{advertiser.campaigns}</td>
+              <td>{advertiser.impressions ? advertiser.impressions : "n/a"}</td>
+              <td>{advertiser.clicks ? advertiser.clicks : "n/a"}</td>
+            </tr>
+          )));
+        } 
+
+        return <div></div>;
+    }
+
+    formatDate(string){
+        return new moment(string).format("DD-MM-YYYY");
+      }
+
+      valueComparer(key, order='asc') {
+        return function(a, b) {
+          if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {          
+            return 0;
+          }
+  
+          const varA = (typeof a[key] === 'string') ?
+            a[key].toUpperCase() : a[key];
+          const varB = (typeof b[key] === 'string') ?
+            b[key].toUpperCase() : b[key];
+  
+          let comparison = 0;
+          if (varA > varB) {
+            comparison = 1;
+          } else if (varA < varB) {
+            comparison = -1;
+          }
+          return (
+            (order === 'desc') ? (comparison * -1) : comparison
+          );
+        };
+      }
+}
+
+export default AdvertiserTableBodyComponent;
